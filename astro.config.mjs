32e5c239481a8861bd78f4bb529abb39d21678ac
iamnotmega <iamnotmega@proton.me>
2025-09-20 20:07:19 +0300

    Initial website

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Reskin',
+			logo: {
+				src: './src/assets/logo.svg',
+				replacesTitle: true,
+			},
+			social: {
+				github: 'https://github.com/iamnotmega/reskin',
+			},
+			sidebar: [
+				{
+					label: 'Quick Start',
+					items: [
+						{ label: 'What is Reskin?', slug: 'guides/quick-start/about' },
+						{ label: 'Prerequisites', slug: 'guides/quick-start/prerequisites' },
+						{ label: 'Installation', slug: 'guides/quick-start/installation' },
+					],
+				},
+			],
+		}),
+	],
+});
